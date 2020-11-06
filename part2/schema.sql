2f6d2c73d8caf3fd42c8e56cf914fcddf43412db
gnl10 <goncalo_mao_cheia@hotmail.com>
2020-11-06 09:46:40 +0000

    more tables added

--- part2/schema.sql
+++ part2/schema.sql
@@ -3,6 +3,9 @@
 drop table if exists person cascade;
 drop table if exists supervisor cascade;
 drop table if exists analyst cascade;
+drop table if exists substation cascade;
+drop table if exists element cascade;
+drop table if exists line cascade;
 
 ----------------------------------------
 -- Table Creation
@@ -27,20 +30,64 @@ address VARCHAR(255) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 tax_id VARCHAR(20) NOT NULL,
 PRIMARY KEY (name, address),
-UNIQUE(phone, tax_id)
+UNIQUE (phone),
+UNIQUE (tax_id)
+--	Every person must exist either in the table 'supervisor' or in the table 'analyst'
 );
 
 CREATE TABLE supervisor (
 name VARCHAR(80) NOT NULL,
-PRIMARY KEY(name),
-FOREIGN KEY(name) REFERENCES person(name)
+address VARCHAR(255) NOT NULL,
+PRIMARY KEY(name, address),
+FOREIGN KEY(name, address) REFERENCES person(name, address)
 );
 
 CREATE TABLE analyst (
 name VARCHAR(80) NOT NULL,
-PRIMARY KEY(name),
-FOREIGN KEY (name) REFERENCES person(name)
+address VARCHAR(255) NOT NULL,
+PRIMARY KEY(name, address),
+FOREIGN KEY(name, address) REFERENCES person(name, address)
 );
+
+
+CREATE TABLE substation (
+gps_coords VARCHAR(80) NOT NULL, -- TODO o prof disse na aula um data type que se adequava a coordenadas
+                                 -- TODO com o data type 'point' nao da, nao o deixa ser primary key
+locality_name VARCHAR(80) NOT NULL,
+PRIMARY KEY (gps_coords)
+);
+
+
+
+CREATE TABLE element(
+id INTEGER NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE line (
+id INTEGER NOT NULL,
+impedance NUMERIC(8,2) NOT NULL,  -- TODO o prof nao precisa de ter casas decimais
+PRIMARY KEY (id),
+FOREIGN KEY (id) REFERENCES element(id)
+);
+
+CREATE TABLE bus_bar (
+id INTEGER NOT NULL,
+voltage NUMERIC(10,2) NOT NULL,  	--RANDOM numeric 10 choice here
+PRIMARY KEY (id),
+FOREIGN KEY (id) REFERENCES element(id)
+);
+
+CREATE TABLE transformer (
+id INTEGER NOT NULL,
+primary_voltage NUMERIC(10,2) NOT NULL,	--RANDOM numeric 10 choice here
+secondary_voltage NUMERIC(10,2)  NOT NULL,	--RANDOM numeric 10 choice here
+gps_coords NUMERIC(.....) ……                               #Notar que lat é num(8,6), long num(9,6)
+PRIMARY KEY (id),
+FOREIGN KEY (id) REFERENCES element(id)
+FOREIGN KEY (gps_coords) REFERENCES substation(gps_coords)
+);
+
 
 
 ----------------------------------------
