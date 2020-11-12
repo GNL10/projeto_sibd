6bca2f04371ac0bd421e082b3fbb480575b1f4f5
Diana Margarida Da Silva Fonseca <ist186967@sigma00.ist.utl.pt>
2020-11-12 19:47:08 +0000

    Inserted new values

--- part2/populate.sql
+++ part2/populate.sql
@@ -18,6 +18,7 @@ insert into person values ('Martin', 'Braga', '999999901', '011');
 insert into person values ('Nguyen', 'Castelo Branco', '9999999902', '012');
 insert into person values ('Oliver', 'Oporto', '9999999903', '013');
 insert into person values ('Parker', 'Lisbon', '9999999904', '014');
+insert into person values ('Antoin', 'Patagonia', '9995999904', '018');
 
 -- Populate Analyst
 insert into analyst values ('Adams', 'Lisbon');
@@ -28,7 +29,7 @@ insert into analyst values ('Evans', 'Coimbra');
 insert into analyst values ('Flores', 'Braga');
 insert into analyst values ('Gonzalez', 'Faro');
 insert into analyst values ('Iacocca', 'Coimbra');
-
+insert into analyst values ('Antoin', 'Patagonia');
 
 -- Populate Supervisor
 insert into supervisor values ('Johnson', 'Lisbon');
@@ -47,22 +48,19 @@ insert into substation values (38.6977695,-9.2094318, 'Lisbon', 'Parker', 'Lisbo
 insert into substation values (40.640496,-18.6537841, 'Aveiro', 'King', 'Aveiro');
 insert into substation values (41.5228712,-7.5465333, 'Vila Real', 'Lopez', 'Vila Real');
 insert into substation values (41.1494512,-8.6107884, 'Oporto', 'Oliver', 'Oporto');
-insert into substation values (41.1543999,-8.5750348, 'Oporto', 'Oliver', 'Oporto');
-insert into substation values (41.1543999,-8.5750348, 'Oporto', 'Martin', 'Braga');
-insert into substation values (41.1543999,-8.5750348, 'Oporto', 'Lopez', 'Vila Real');
+insert into substation values (41.1543991,-8.5750341, 'Oporto', 'Oliver', 'Oporto');
+insert into substation values (41.1523592,-8.5150342, 'Oporto', 'Martin', 'Braga');
+insert into substation values (41.1513993,-8.5752343, 'Oporto', 'Lopez', 'Vila Real');
 
 -- Populate element
 insert into element values ('B-111');
 insert into element values ('B-114');
-
 insert into element values ('B-200');
 insert into element values ('B-201');
 insert into element values ('B-202');
 insert into element values ('B-203');
 insert into element values ('B-204');
 insert into element values ('B-205');
-
-
 insert into element values ('B-300');
 insert into element values ('B-301');
 insert into element values ('B-302');
@@ -75,14 +73,20 @@ insert into element values ('B-308');
 insert into element values ('B-309');
 insert into element values ('B-310');
 insert into element values ('B-311');
-
 insert into element values ('B-789');
+insert into element values ('B-457');
+insert into element values ('B-371');
+insert into element values ('B-159');
+
 
 
 -- Populate line
 insert into line values ('B-789', 1000);
 insert into line values ('B-111', 4);
 insert into line values ('B-114', 500);
+insert into line values ('B-457', 180);
+insert into line values ('B-371', 47);
+insert into line values ('B-159', 510);
 
 -- Populate bus_bar
 insert into bus_bar values ('B-300', 230);
@@ -113,6 +117,8 @@ insert into incident values ('23-AUG-2018 13:20:16', 'B-789', 'Arvore na linha',
 insert into incident values ('23-JUN-2019 09:00:01', 'B-111', 'Ramo na linha', 2);
 insert into incident values ('01-OCT-2015 23:55:16', 'B-114', 'Descarga atmosferica', 9);
 insert into incident values ('29-JAN-2017 10:32:12', 'B-303', 'Fumo a sair da bus_bar', 7);
+insert into incident values ('25-MAR-2017 06:29:41', 'B-309', 'Interrupcao da rede eletrica', 8);
+
 
 -- Populate line_incident
 insert into line_incident values ('08-JAN-2019 10:35:02', 'B-789', 10);
@@ -127,7 +133,12 @@ insert into analyses values ('Cook', 'Lisbon', '02-JUL-2019 22:23:11', 'Tempesta
 insert into analyses values ('Cook', 'Lisbon', '23-AUG-2018 13:20:16', 'Vento forte derrubou arvore', 'B-789');
 insert into analyses values ('Adams', 'Lisbon', '23-JUN-2019 09:00:01', 'Ramo na linha', 'B-111');
 insert into analyses values ('Cook', 'Lisbon', '29-JAN-2017 10:32:12', 'Inseto no elemento', 'B-303');
+insert into analyses values ('Antoin', 'Patagonia', '25-MAR-2017 06:29:41', 'Descarga Atmosferica', 'B-309');
 
 -- Populate line_connection
 insert into line_connection values ('B-111', 'B-300', 'B-301');
 insert into line_connection values ('B-114', 'B-302', 'B-303');
+insert into line_connection values ('B-457', 'B-303', 'B-302');
+insert into line_connection values ('B-371', 'B-303', 'B-311');
+insert into line_connection values ('B-371', 'B-305', 'B-310');
+
