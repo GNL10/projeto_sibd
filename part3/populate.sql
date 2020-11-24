e28977296378026ba43e1493effab3598bdac609
gnl10 <goncalo_mao_cheia@hotmail.com>
2020-11-24 00:31:18 +0000

    Finished the two queries left and populate some more

--- part3/populate.sql
+++ part3/populate.sql
@@ -4,14 +4,20 @@ insert into person values ('John', 'Lisbon', '931234567', '0897567891231521');
 
 insert into person values ('Maria', 'Aveiro', '932135711', '182424712312356');
 insert into person values ('Lewis', 'Amadora', '910984213', '129384512356573121');
+insert into person values ('Ricardo', 'Sintra', '913859231', '0012395123904657');
+insert into person values ('Adams', 'Sintra', '921874729', '4673925874612');
+
 
 -- Populating Supervisor
 insert into supervisor values ('Maria', 'Aveiro');
 insert into supervisor values ('Lewis', 'Amadora');
+insert into supervisor values ('Ricardo', 'Sintra');
+insert into supervisor values ('Adams', 'Sintra');
 
 -- Populating Analyst
 insert into analyst values ('Adams', 'Lisbon');
 insert into analyst values ('John', 'Lisbon');
+insert into analyst values ('Adams', 'Sintra');
 
 -- Populating substation
 insert into substation values (40.640507, -8.653754, 'Aveiro', 'Maria', 'Aveiro');
@@ -21,13 +27,15 @@ insert into substation values (38.762329, -9.239910, 'Amadora', 'Lewis', 'Amador
 insert into element values ('B-789');
 insert into element values ('B-111');
 
-insert into element values ('B-300');
-insert into element values ('B-301');
-
 insert into element values ('B-200');
 insert into element values ('B-201');
 insert into element values ('B-202');
 insert into element values ('B-203');
+
+insert into element values ('B-300');
+insert into element values ('B-301');
+
+
 
 -- Populating Busbar
 insert into busbar values ('B-200', 230);
@@ -41,9 +49,18 @@ insert into transformer values ('B-301', 200, 600, 38.762329, -9.239910, 'B-202'
 
 -- Populating Incident
 insert into incident values ('08-JAN-2019 10:35:02', 'B-789', 'Element stopped working after a spark', 'High');
+insert into incident values ('22-FEB-2019 15:22:05', 'B-789', 'Element malfunction', 'High');
+insert into incident values ('15-APR-2019 12:01:55', 'B-789', 'Element caught fire', 'Medium');
+
 insert into incident values ('21-JUL-2018 01:11:18', 'B-789', 'Element is making an unusual noise', 'Low');
+insert into incident values ('24-SEP-2019 12:14:12', 'B-200', 'Complete element failure', 'High');
+insert into incident values ('01-OCT-2019 20:33:51', 'B-201', 'Mechanical damage', 'Medium');
 
 
 -- Populating Analyses
 insert into analyses values ('08-JAN-2019 10:35:02', 'B-789', 'Squirrel died on the element', 'Adams', 'Lisbon');
+insert into analyses values ('22-FEB-2019 15:22:05', 'B-789', 'Insulation was old', 'Adams', 'Lisbon');
+insert into analyses values ('15-APR-2019 12:01:55', 'B-789', 'Unexpected ground connection', 'Adams', 'Sintra');
+
+
 insert into analyses values ('21-JUL-2018 01:11:18', 'B-789', 'Broken support caused the problem', 'John', 'Lisbon');
--- part3/queries.sql
+++ part3/queries.sql
@@ -1,17 +1,31 @@
+-- TODO duvida - Na query 3 temos que incluir elementos que nao tem incidentes?
+
 -- 1. Who are the analysts that have analyzed every incident of element ‘B-789’?
-select a.name
+select distinct a.name, a.address
 from analyses a
 where id = 'B-789';
 
 -- 2. Who are the supervisors that do not supervise substations south of Rio Maior (Portugal) (Rio Maior coordinates: 39.336775, -8.936379 (cf. Google Maps)?
-select name
+select name, address -- TODO tem que ter address, porque podem existir names iguais com addresses diferentes!
 from supervisor
 except
-select sup.name
+select sup.name, sup.address
 from substation s
 join supervisor sup on sup.name = s.sname and sup.address = s.saddress
 where s.gpslat <= 39.336775;
 
 -- 3. What are the elements with the smallest amount of incidents?
+select id, count(*)
+from incident
+group by id
+having count(*) <= ALL (
+    select count(*)
+    from incident
+    group by id
+    );
 
 -- 4. How many substations does each supervisor supervise? (include supervisors that do not supervise any at the moment)
+select sup.name, sup.address, count(sub.gpslat)
+from supervisor sup
+left outer join substation sub on sup.name = sub.sname and sup.address = sub.saddress
+group by sup.name, sup.address;
