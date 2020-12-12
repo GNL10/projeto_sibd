894d9620bd0037505b1544b33189e226ee24cd03
BeatrizC14 <beatriz.cabarrao@gmail.com>
2020-12-12 01:09:32 +0000

    Adicionei Analytics

--- part3/etl.sql
+++ part3/etl.sql
@@ -122,3 +122,9 @@ from incident i
 ;
 
 select * from f_incident;
+
+--Using Rollup
+select severity, locality, week_day, count(*)
+from f_incident natural join d_location
+    natural join d_time
+group by rollup (severity, locality, week_day);
